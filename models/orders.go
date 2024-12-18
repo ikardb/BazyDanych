18ae66aec63f88cd1cb878e62baa260f0c30c12b
Michał Kostański <m.kostanski3@gmail.com>
2024-12-18 22:47:34 +0100

    Dodano: dokladne wyswietlanie zamowienia, zliczanie kosztu zamowienia, opcje migracji produktow z zamowienia do stanu magazynowego

--- models/orders.go
+++ models/orders.go
@@ -3,11 +3,12 @@ package models
 import "time"
 
 type Orders struct {
-	Id_zamowienia    int16     `gorm:"primaryKey;autoIncrement" json:"id_zamowienia"`
-	Id_sklepu        int16     `json:"id_sklepu"`
-	Id_uzytkownika   *int16    `json:"id_uzytkownika"`
-	Data_zamowienia  time.Time `json:"data_zamowienia"`
-	Koszt_zamowienia float64   `json:"koszt_zamowienia"`
+	Id_zamowienia     int16     `gorm:"primaryKey;autoIncrement" json:"id_zamowienia"`
+	Id_sklepu         int16     `json:"id_sklepu"`
+	Id_uzytkownika    *int16    `json:"id_uzytkownika"`
+	Data_zamowienia   time.Time `json:"data_zamowienia"`
+	Koszt_zamowienia  float64   `json:"koszt_zamowienia"`
+	Wczytane_do_stanu bool      `json:"wczytane_do_stanu"`
 }
 
 func (Orders) TableName() string {
